f7a74449326f92bda1b83c7ebe960a6b45333b84
sunnyleeyun <sunnglee945@hotmail.com>
2018-09-27 22:10:35 +0800

    Add MVC first commit

---
+++ mvcdemo/MVCDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCDemo
+//
+//  Created by 李昀 on 2018/9/27.
+//  Copyright © 2018 李昀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
